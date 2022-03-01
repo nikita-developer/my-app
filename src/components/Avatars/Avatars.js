adf736f15136555df745a99b55d0a3b6bfff23e9
nikita-job <nikitka.kuznetsov.1994@mail.ru>
2022-03-01 17:04:37 +0300

    разобрался с выводом списка аватарок компонент Avatars

--- src/components/Avatars/Avatars.js
+++ src/components/Avatars/Avatars.js
@@ -6,23 +6,41 @@ class Avatars extends Component {
     constructor() {
         super()
         this.state = {
-            name: 'Nikol',
-            country: 'USA',
-            img: '/media/Avatars/logo.png',
+            human: [
+                {
+                    name: 'Nikol',
+                    country: 'USA',
+                    img: '/media/Avatars/logo.png',
+                },
+                {
+                    name: 'Nikol2',
+                    country: 'USA2',
+                    img: '/media/Avatars/logo.png',
+                },
+                {
+                    name: 'Nikol3',
+                    country: 'USA3',
+                    img: '/media/Avatars/logo.png',
+                },
+            ]
         }
     }
 
     render() {
-        return(
+        const avtarItem = this.state.human.map((item, key) =>
+            <div className="avatars__item" key={key}>
+                <div className="avatars__img">
+                    <img src={item.img}/>
+                </div>
+                <div className="avatars__name">{item.name}</div>
+                <div className="avatars__country">{item.country}</div>
+            </div>
+        );
+
+        return (
             <div className="avatars">
                 <div className="avatars__wrapper">
-                    <div className="avatars__item">
-                        <div className="avatars__img">
-                            <img src={this.state.img} />
-                        </div>
-                        <div className="avatars__name">{this.state.name}</div>
-                        <div className="avatars__country">{this.state.country}</div>
-                    </div>
+                    {avtarItem}
                 </div>
             </div>
         )

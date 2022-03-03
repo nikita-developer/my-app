350b26874537e37e56382468b5e5494e9547de0f
nikita-job <nikitka.kuznetsov.1994@mail.ru>
2022-03-03 09:23:42 +0300

    Добавил счетчик (тренировка)

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 
+import Count from '../../Count/Count';
 import './Home.css'
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <Count />
+        </div>
     )
 }
 

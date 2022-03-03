7bbb977d0ae124dcb50a45552c72b3ddd9624e08
nikita-job <nikitka.kuznetsov.1994@mail.ru>
2022-03-03 13:31:20 +0300

    научился добавлять аватарки в стейт human

--- src/components/Avatars/Avatars.js
+++ src/components/Avatars/Avatars.js
@@ -3,8 +3,8 @@ import React, {Component} from "react"
 import './Avatars.css'
 
 class Avatars extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             human: [
                 {
@@ -22,28 +22,61 @@ class Avatars extends Component {
                     country: 'USA3',
                     img: '/media/Avatars/logo.png',
                 },
-            ]
+            ],
+            addAvatarImg: '',
+            addAvatarName: '',
+            addAvatarCountry: '',
         }
+
+        this.addAvatar = this.addAvatar.bind(this)
+        this.inputChange = this.inputChange.bind(this)
     }
 
     render() {
-        const avtarItem = this.state.human.map((item, key) =>
-            <div className="avatars__item" key={key}>
-                <div className="avatars__img">
-                    <img src={item.img}/>
-                </div>
-                <div className="avatars__name">{item.name}</div>
-                <div className="avatars__country">{item.country}</div>
-            </div>
-        );
-
         return (
             <div className="avatars">
                 <div className="avatars__wrapper">
-                    {avtarItem}
+                    <div className="avatars__head">
+                        <input className="avatars__field" value={this.state.addAvatarName} onChange={this.inputChange}/>
+                        <button className="avatars__btn" onClick={this.addAvatar}>Добавить</button>
+                    </div>
+                    <div className="avatars__body">
+                        {this.avtarItem()}
+                    </div>
                 </div>
             </div>
         )
+    }
+
+    addAvatar() {
+        if(Boolean(this.state.addAvatarName.trim())) {
+            this.setState({
+                human: [...this.state.human, {
+                    name: this.state.addAvatarName,
+                    country: 'USA3',
+                    img: '/media/Avatars/logo.png',
+                }]
+            })
+        }
+    }
+
+    inputChange(e) {
+        this.setState({
+            addAvatarName: e.target.value
+        })
+    }
+
+    avtarItem() {
+        return (this.state.human.map((item, key) =>
+                <div className="avatars__item" key={key}>
+                    <div className="avatars__img">
+                        <img src={item.img} alt="" />
+                    </div>
+                    <div className="avatars__name">{item.name}</div>
+                    <div className="avatars__country">{item.country}</div>
+                </div>
+            )
+        );
     }
 }
 

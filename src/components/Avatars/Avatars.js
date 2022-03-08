700d8dd16461ad0788ee92dc0f82e3e9fb603ef1
Nikita <nikitka.kuznetsov.1994@mail.ru>
2022-03-08 22:25:56 +0300

    вроде разобрался с подключение базы данных

--- src/components/Avatars/Avatars.js
+++ src/components/Avatars/Avatars.js
@@ -1,72 +1,53 @@
 
-import React, {Component} from "react";
-import './Avatars.scss';
-// import axios from "axios";
-// axios.get('https://api-maps.yandex.ru/2.1/?apikey=ваш API-ключ&lang=ru_RU').then((response) => {
-//     console.log(response);
-// });
-
-// Имя сервера БД - server251.hosting.reg.ru
-// Имя пользователя (логин) БД - u1305374_spas
-// Пароль пользователя БД - govno1994
-// Имя БД - u1305374_spas
+import React, {Component} from "react"
+import './Avatars.scss'
+import dataAvatars from './dataAvatars.json'
+import axios from "axios"
 
 class Avatars extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataAvatars: dataAvatars.users
+        }
+    }
+
+    // componentDidMount() {
+    //     axios.get(dataAvatars).then(res => {
+    //         this.setState({
+    //             users: res.data.users,
+    //         })
+    //     });
+    // }
+
     render() {
-        return (
-            <div className="avatars">
-                <div className="avatars__body">
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
-                    <figure className="avatars__card">
-                        <div className="avatars__media"><img src="images/thumb3.jpg" alt="name" /></div>
-                        <figcaption className="avatars__name">Купеческий клуб</figcaption>
-                    </figure>
+        if(this.state.dataAvatars) {
+            return (
+                <div className="avatars">
+                    <div className="avatars__body">
+                        {this.avtarItem()}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div className="avatars">
+                    <div className="avatars__body">
+                        <div>1234</div>
+                    </div>
+                </div>
+            )
+        }
+    }
+
+    avtarItem() {
+        return (this.state.dataAvatars.map((user, key) =>
+                <figure className="avatars__card" key={key}>
+                    <div className="avatars__media"><img src={user.img} alt="name" /></div>
+                    <figcaption className="avatars__name">{user.name}</figcaption>
+                </figure>
+            )
+        );
     }
 }
 
